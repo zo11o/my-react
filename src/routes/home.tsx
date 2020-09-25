54d701ce2815b3d1bbefb31bbf9c405d29d04709
zo11o <zhuowenjian@inboyu.com>
2020-09-25 18:04:49 +0800

    feat(router): home

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,4 +1,23 @@
 import Loadable from 'react-loadable';
+import React from 'react';
+
+interface IArguments {
+  isLoading: boolean;
+  error: any;
+}
+
+const MyLoadingComponent = ({ isLoading, error }: IArguments) => {
+  // Handle the loading state
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  // Handle the error state
+  else if (error) {
+    return <div>Sorry, there was a problem loading the page.</div>;
+  } else {
+    return null;
+  }
+};
 
 const Home = Loadable({
   loader: () => import('../views/home'),

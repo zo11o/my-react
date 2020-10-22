907cf07e2480bd6aa2c9c6f9d0ec25e0b986fee3
zo11o <zhuowenjian@inboyu.com>
2020-10-22 10:05:48 +0800

    feat(muise-master): 创建musie-master

--- src/routes/user.tsx
+++ src/routes/user.tsx
@@ -1,12 +1,5 @@
 import Loadable from 'react-loadable';
 import LoadingComponent from './loading-component';
-// interface IArguments {
-//   isLoading: boolean;
-//   error: any;
-// }
-
-// const Home = () => <div>home</div>
-// const Loading = (a,b) => <div>sdf</div>;
 
 export const User = Loadable({
   loader: () => import('../views/user'),
